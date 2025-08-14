5e9e8a9ed2d51cf775f13d0fddc3fb08fa556eff
sagrawal11 <agrawal.sarthak@gmail.com>
2025-08-14 10:33:30 -0500

    running tracknet training properly now

--- src/ball_tracker_pytorch.py
+++ src/ball_tracker_pytorch.py
@@ -47,73 +47,8 @@ class ConvBlock(nn.Module):
     def forward(self, x):
         return self.block(x)
 
-class BallTrackerNet(nn.Module):
-    """PyTorch TrackNet model for ball tracking"""
-    def __init__(self, out_channels=256):
-        super().__init__()
-        self.out_channels = out_channels
-
-        # Encoder
-        self.conv1 = ConvBlock(in_channels=9, out_channels=64)
-        self.conv2 = ConvBlock(in_channels=64, out_channels=64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = ConvBlock(in_channels=64, out_channels=128)
-        self.conv4 = ConvBlock(in_channels=128, out_channels=128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv5 = ConvBlock(in_channels=128, out_channels=256)
-        self.conv6 = ConvBlock(in_channels=256, out_channels=256)
-        self.conv7 = ConvBlock(in_channels=256, out_channels=256)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv8 = ConvBlock(in_channels=256, out_channels=512)
-        self.conv9 = ConvBlock(in_channels=512, out_channels=512)
-        self.conv10 = ConvBlock(in_channels=512, out_channels=512)
-        
-        # Decoder
-        self.ups1 = nn.Upsample(scale_factor=2)
-        self.conv11 = ConvBlock(in_channels=512, out_channels=256)
-        self.conv12 = ConvBlock(in_channels=256, out_channels=256)
-        self.conv13 = ConvBlock(in_channels=256, out_channels=256)
-        self.ups2 = nn.Upsample(scale_factor=2)
-        self.conv14 = ConvBlock(in_channels=256, out_channels=128)
-        self.conv15 = ConvBlock(in_channels=128, out_channels=128)
-        self.ups3 = nn.Upsample(scale_factor=2)
-        self.conv16 = ConvBlock(in_channels=128, out_channels=64)
-        self.conv17 = ConvBlock(in_channels=64, out_channels=64)
-        self.conv18 = ConvBlock(in_channels=64, out_channels=self.out_channels)
-
-        self.softmax = nn.Softmax(dim=1)
-        self._init_weights()
-                  
-    def forward(self, x, testing=False): 
-        batch_size = x.size(0)
-        x = self.conv1(x)
-        x = self.conv2(x)    
-        x = self.pool1(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.pool2(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
-        x = self.pool3(x)
-        x = self.conv8(x)
-        x = self.conv9(x)
-        x = self.conv10(x)
-        x = self.ups1(x)
-        x = self.conv11(x)
-        x = self.conv12(x)
-        x = self.conv13(x)
-        x = self.ups2(x)
-        x = self.conv14(x)
-        x = self.conv15(x)
-        x = self.ups3(x)
-        x = self.conv16(x)
-        x = self.conv17(x)
-        x = self.conv18(x)
-        out = x.reshape(batch_size, self.out_channels, -1)
-        if testing:
-            out = self.softmax(out)
-        return out                       
+# Import the original model architecture
+from model_original import BallTrackerNet                       
     
     def _init_weights(self):
         for module in self.modules():
